0f9a1d7e18504d15fa369be79716c98ad2336a7d
asivn <asiansinquiries@gmail.com>
2025-10-02 00:57:08 -0700

    trying again

--- lua/nvchad/configs/cmp.lua
+++ lua/nvchad/configs/cmp.lua
@@ -1,58 +1,146 @@
-dofile(vim.g.base46_cache .. "cmp")
-
-
 local cmp = require "cmp"
 
-local options = {
-  completion = { completeopt = "menu,menuone" },
+dofile(vim.g.base46_cache .. "cmp")
 
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
+local cmp_ui = require("nvconfig").ui.cmp
+local cmp_style = cmp_ui.style
 
-  mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-
-    ["<CR>"] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    },
-
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
-        require("luasnip").expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
-        require("luasnip").jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  },
-
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "nvim_lua" },
-    { name = "async_path" },
-  },
+local field_arrangement = {
+    atom = { "kind", "abbr", "menu" },
+    atom_colored = { "kind", "abbr", "menu" },
 }
 
-return vim.tbl_deep_extend("force", options, require "nvchad.cmp")
+local formatting_style = {
+    -- default fields order i.e completion word + item.kind + item.kind icons
+    fields = field_arrangement[cmp_style] or { "abbr", "kind", "menu" },
+
+    format = function(_, item)
+        local icons = require "nvchad.icons.lspkind"
+        local icon = (cmp_ui.icons and icons[item.kind]) or ""
+
+        if cmp_style == "atom" or cmp_style == "atom_colored" then
+            icon = " " .. icon .. " "
+            item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
+            item.kind = icon
+        else
+            icon = cmp_ui.lspkind_text and (" " .. icon .. " ") or icon
+            item.kind = string.format("%s %s", icon, cmp_ui.lspkind_text and item.kind or "")
+        end
+
+        return item
+    end,
+}
+
+local function border(hl_name)
+    return {
+        { "╭", hl_name },
+        { "─", hl_name },
+        { "╮", hl_name },
+        { "│", hl_name },
+        { "╯", hl_name },
+        { "─", hl_name },
+        { "╰", hl_name },
+        { "│", hl_name },
+    }
+end
+
+local options = {
+    completion = {
+        completeopt = "menu,menuone",
+    },
+
+    window = {
+        completion = {
+            side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
+            winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
+            scrollbar = false,
+        },
+        documentation = {
+            border = border "CmpDocBorder",
+            winhighlight = "Normal:CmpDoc",
+        },
+    },
+
+    formatting = formatting_style,
+
+    mapping = {
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+
+        ["<CR>"] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+        },
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    },
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "path" },
+        { name = "vimtex" },
+    },
+
+    -- `/` cmdline setup.
+    cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+            { name = 'buffer' }
+        },
+    }),
+
+    cmp.setup.filetype("tex", {
+        sources = {
+            { name = 'vimtex' },
+        }
+    }),
+
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+            { name = 'path' },
+            { name = 'cmdline' }
+        },
+        formatting = {
+            fields = { "abbr", "kind", "menu" },
+            format = function(entry, vim_item)
+                vim_item.kind = string.format("%s", require("nvchad.icons.lspkind")[vim_item.kind]) -- Kind icons
+                vim_item.menu = ({
+                    vimtex = vim_item.menu,
+                    nvim_lsp = "[LSP]",
+                    spell = "[Spell]",
+                    -- orgmode = "[Org]",
+                    -- latex_symbols = "[Symbols]",
+                    cmdline = "[Command]",
+                    path = "[Path]",
+                })[entry.source.name]
+                return vim_item
+            end,
+        },
+    }),
+}
+
+if cmp_style ~= "atom" and cmp_style ~= "atom_colored" then
+    options.window.completion.border = border "CmpBorder"
+end
+
+return options
