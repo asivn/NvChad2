d603fcd9b6759fbc0cab4418fcae3fdb6d2fc856
asivn <asiansinquiries@gmail.com>
2025-10-01 23:22:34 -0700

    Update options.lua

--- lua/nvchad/options.lua
+++ lua/nvchad/options.lua
@@ -2,10 +2,12 @@ local opt = vim.opt
 local o = vim.o
 local g = vim.g
 
+-------------------------------------- globals -----------------------------------------
+g.toggle_theme_icon = "   "
+
 -------------------------------------- options ------------------------------------------
 o.laststatus = 3
 o.showmode = false
-o.splitkeep = "screen"
 
 o.clipboard = "unnamedplus"
 o.cursorline = true
@@ -13,10 +15,10 @@ o.cursorlineopt = "number"
 
 -- Indenting
 o.expandtab = true
-o.shiftwidth = 2
+o.shiftwidth = 4
 o.smartindent = true
-o.tabstop = 2
-o.softtabstop = 2
+o.tabstop = 4
+o.softtabstop = 4
 
 opt.fillchars = { eob = " " }
 o.ignorecase = true
@@ -40,13 +42,10 @@ o.undofile = true
 -- interval for writing swap file to disk, also used by gitsigns
 o.updatetime = 250
 
--- go to previous/next line with h,l,left arrow and right arrow
--- when cursor reaches end/beginning of line
-opt.whichwrap:append "<>[]hl"
+-- g.mapleader = " "
 
 -- disable some default providers
 g.loaded_node_provider = 0
-g.loaded_python3_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
 
